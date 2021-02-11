dd4fb1ff043e86ae645e5b5379477fb35e2db310
sjs <seo777111@gmail.com>
2021-02-11 16:32:20 +0900

    [검색폼 구현] reset 클릭, 검색어 지울 경우 MainController 에게 이벤트를 위임한다.

--- 1-vanilla/js/controllers/MainController.js
+++ 1-vanilla/js/controllers/MainController.js
@@ -5,5 +5,15 @@ const tag = '[MainController]'
 export default {
   init() {
     FormView.setup(document.querySelector('form'))
+        .on('@submit',e => this.onSubmit(e.detail.input))
+        .on('@reset',e => this.onResetForm())
   },
+
+  onSubmit(input) {
+    console.log(tag, 'onDSubmit()', input)
+  },
+
+  onResetForm() {
+    console.log(tag, 'onReset()')
+  }
 }
--- 1-vanilla/js/views/FormView.js
+++ 1-vanilla/js/views/FormView.js
@@ -20,10 +20,27 @@ FormView.showResetBtn = function (show = true) {
 FormView.bindEvents = function () {
   this.on('submit', e => e.preventDefault())
   this.inputEl.addEventListener('keyup', e => this.onKeyup(e))
-}
+  this.resetEl.addEventListener('click',e => this.onClickReset())
+};
 
 FormView.onKeyup = function (e) {
-  this.showResetBtn(this.inputEl.value.length)
-}
+  const enter = 13;
+  this.showResetBtn(this.inputEl.value.length);
+  if (!this.inputEl.value.length) this.emit('@reset');
+  if (e.keyCode !== enter) return;
+  this.emit('@submit',{ input: this.inputEl.value });
+  this.emit('@reset',{ input: this.inputEl.value })
+};
+
+// todo : x버튼을 클릭하거나,
+//  검색어를 삭제하면 검색 결과를 삭제한다. (MaimController에게 위임)
+/*
+* MainController에게 클릭 이벤트를 전달한다 -> mainController에서 받는다...  단순히 그것만?
+*
+* */
+FormView.onClickReset = function () {
+  this.emit('@reset')
+  this.showResetBtn(false)
+};
 
 export default FormView
