b6110e3fbe6acc9e8f3fd9a1063a3537da462153
sjs <seo777111@gmail.com>
2021-02-11 19:09:58 +0900

    [탭 구현] defult tab 설정 및 MainController defult render 호출

--- 1-vanilla/js/controllers/MainController.js
+++ 1-vanilla/js/controllers/MainController.js
@@ -1,6 +1,6 @@
 import FormView from '../views/FormView.js'
 import ResultView from '../views/ResultView.js'
-
+import TabView from "../views/TabView.js";
 import SearchModel from '../models/SearchModel.js'
 
 const tag = '[MainController]'
@@ -9,9 +9,20 @@ export default {
   init() {
     FormView.setup(document.querySelector('form'))
       .on('@submit', e => this.onSubmit(e.detail.input))
-      .on('@reset', e => this.onResetForm())
+      .on('@reset', e => this.onResetForm());
 
+    TabView.setup(document.querySelector('#tabs'));
     ResultView.setup(document.querySelector('#search-result'))
+
+    this.selectTab = '추천 검색어';
+    this.renderView();
+  },
+
+  // View를 그려주는것이 많기때문에 랜더 함수를 뺀다.
+  renderView() {
+    console.log(tag,'renderView()');
+    TabView.setActiveTab(this.selectTab);
+    ResultView.hide();
   },
 
   onSubmit(input) {
